26758e1559f6cde3756a31cd424c980ed6832d5c
forklp <1637108208@qq.com>
2018-05-26 20:37:51 +0800

    first commit

---
+++ html_outputer.py
@@ -0,0 +1,15 @@
+class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        for data in self.datas:
+            print(data['title'].encode('utf-8'))
+            print(" ")
+            
+
